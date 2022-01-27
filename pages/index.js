c3c96f393672b9cb7b7ad144d965b5ad738c0e05
Chris <richardsonchrisj@gmail.com>
2022-01-26 18:26:30 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import { Container, Box, Heading } from "@chakra-ui/react";
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
+        Hello, I&apos;m an Instructional Designer and full-stack developer based
+        in San Diego!
+      </Box>
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Chris Richardson, PhD
+          </Heading>
+          <p>
+            Instructional designer. Web developer. Writer. Recovering academic.
+          </p>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+export default Page;
