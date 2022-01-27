cb5261f8b6929f906c1fba6667d2a967f3b32d94
richardsonchrisj <richardsonchrisj@gmail.com>
2022-01-27 08:25:16 -0800

    macupdate2

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import NextLink from "next/link";
-import { Container, Box, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Section from "../components/section";
 
 const Home = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        mb={6}
+        p={3}
+        align="center"
+      >
         Hello, I&apos;m an Instructional Designer and full-stack developer based
         in San Diego!
       </Box>
+
       <Box display={{ md: "flex" }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
@@ -17,7 +31,30 @@ const Home = () => {
             Instructional designer. Web developer. Writer. Recovering academic.
           </p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/logo-dark.png"
+            alt="Profile image"
+          />
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph.</p>
+      </Section>
     </Container>
   );
 };
