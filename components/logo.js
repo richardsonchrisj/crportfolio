a4357bb570e575433161c32801b0265388966054
Chris <richardsonchrisj@gmail.com>
2022-01-27 13:25:04 -0800

    bio and animation toggle

--- components/logo.js
+++ components/logo.js
@@ -2,32 +2,51 @@ import Link from "next/link";
 import Image from "next/image";
 import { Text, useColorModeValue } from "@chakra-ui/react";
 import styled from "@emotion/styled";
+import { jsx, css, keyframes } from "@emotion/react";
+
+const spin = keyframes`
+  from, 20%, 53%, 80%, to {
+    transform: translate3d(0,0,0);
+  }
+
+  40%, 43% {
+    transform: translate3d(0, -30px, 0);
+  }
+
+  70% {
+    transform: translate3d(0, -15px, 0);
+  }
+
+  90% {
+    transform: translate3d(0,-4px,0);
+  }
+`;
 
 const LogoBox = styled.span`
   font-weight: bold;
   font-size: 18px;
   display: inline-flex;
   align-items: center;
-  height: 30px;
+  height: 75px;
   line-height: 20px;
   padding: 10px;
 
   &:hover img {
-    transform: rotate(20deg);
+    animation: ${spin} 1s ease-in-out infinite;
   }
 `;
 
 const Logo = () => {
-  const batChris = `/images/logo.jpg`;
+  const logo = `/images/logo-dark.png`;
 
   return (
     <Link href="/">
       <a>
         <LogoBox>
           <Image
-            src={batChris}
-            width={100}
-            height={25}
+            src={logo}
+            width={256}
+            height={75}
             alt="BatChris and Rupert Logo"
           />
           <Text
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import NextLink from "next/link";
 import {
   Container,
+  Button,
   Box,
   Heading,
   Image,
   useColorModeValue,
+  Link,
 } from "@chakra-ui/react";
 import Section from "../components/section";
+import Paragraph from "../components/paragraph";
+import { BioSection, BioYear } from "../components/bio";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 const Home = () => {
   return (
@@ -15,6 +20,7 @@ const Home = () => {
         borderRadius="lg"
         bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
         mb={6}
+        mt={10}
         p={3}
         align="center"
       >
@@ -44,7 +50,7 @@ const Home = () => {
             maxWidth="100px"
             display="inline-block"
             borderRadius="full"
-            src="/images/logo-dark.png"
+            src="/images/logo.jpg"
             alt="Profile image"
           />
         </Box>
@@ -53,7 +59,75 @@ const Home = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>Paragraph.</p>
+        <Paragraph>
+          Hi, I'm Chris. I'm a Senior Instructional Designer and Web Developer.
+          For a decade, I taught Media and Cultural Studies at a small liberal
+          arts college outside Atlanta. Now, I'm based in San Diego, where I
+          help professionals learn and collaborate more effectively through
+          innovative, evidence-based design.
+          <NextLink href="/works/inkdrop">
+            <Link>InkDrop</Link>
+          </NextLink>
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1985</BioYear>
+            Born in Toronto
+          </BioSection>
+          <BioSection>
+            <BioYear>2007</BioYear>
+            Completed Bachelor of Journalism, Ryerson University
+          </BioSection>
+          <BioSection>
+            <BioYear>2008</BioYear>
+            Completed Master of Arts, Brock University
+          </BioSection>
+          <BioSection>
+            <BioYear>2012</BioYear>
+            Completed PhD, Western University
+          </BioSection>
+          <BioSection>
+            <BioYear>2012</BioYear>
+            Hired as Assistant Professor, Young Harris College
+          </BioSection>
+          <BioSection>
+            <BioYear>2015</BioYear>
+            Promoted to Department Chair, Young Harris College
+          </BioSection>
+          <BioSection>
+            <BioYear>2019</BioYear>
+            Promoted to Associate Professor, Young Harris College
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Hired as Senior Instructional Designer, Pragmatic Institute
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Completed Google Data Analytics Certificate
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Completed Professional Certificate in Coding, MIT
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ❤️
+          </Heading>
+          <Paragraph>Art, Music</Paragraph>
+        </Section>
       </Section>
     </Container>
   );
